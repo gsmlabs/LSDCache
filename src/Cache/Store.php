85a1ce3afad105144158d79a5c8141f8dfd1cb92
Przemek Sobstel <przemek@globalsportsmedia.com>
2011-03-07 12:11:40 +0100

    Phpdoc for Cache_Store.

--- src/Cache/Store.php
+++ src/Cache/Store.php
@@ -1,8 +1,32 @@
 <?php
 interface Cache_Store {
 
-  public function set($key, $value, $ttl = 0);
+  /**
+   * @param string $key
+   * @return mixed Value or FALSE if not exists
+   */
   public function get($key);
+
+  /**
+   * @param string $key
+   * @param mixed $value
+   * @param int $ttl
+   * @return bool
+   */
+  public function set($key, $value, $ttl = 0);
+
+  /**
+   * @param string $key
+   * @param mixed $value
+   * @param int $ttl
+   * @return bool
+   */
   public function add($key, $value, $ttl = 0);
+
+  /**
+   * @param string $key
+   * @return bool
+   */
+  public function delete($key);
 
 }
