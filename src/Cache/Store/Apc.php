ad09d102bf69b39a45219b64531e257ef382a5cc
Przemek Sobstel <przemek@globalsportsmedia.com>
2011-03-07 12:12:35 +0100

    Store:Apc: add delete method.

--- src/Cache/Store/Apc.php
+++ src/Cache/Store/Apc.php
@@ -1,15 +1,20 @@
 <?php
 class Cache_Store_Apc implements Cache_Store {
 
-  public function set($key, $value, $ttl = 0) {
-    return apc_store($key, $value, $ttl);
-  }
-
   public function get($key) {
     return apc_fetch($key);
   }
 
+  public function set($key, $value, $ttl = 0) {
+    return apc_store($key, $value, $ttl);
+  }
+
   public function add($key, $value, $ttl = 0) {
+    return apc_add($key, $value, $ttl);
+  }
+
+  public function delete($key) {
+    return apc_delete($key);
   }
 
 }
