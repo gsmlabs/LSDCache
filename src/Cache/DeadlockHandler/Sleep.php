58405522de353e9c5f635dda11e575fb49704ae9
Przemek Sobstel <przemek@globalsportsmedia.com>
2011-05-04 15:02:15 +0200

    Sleep deadlock handler: fix minor errors and typos.

--- src/Cache/DeadlockHandler/Sleep.php
+++ src/Cache/DeadlockHandler/Sleep.php
@@ -18,13 +18,13 @@ class Cache_DeadlockHandler_Sleep implements Cache_DeadlockHandler {
     if (!isset($this->counters[$key])) {
       $this->counters[$key] = 0;
     }
-    
-    if ($this->counters[$key] > $max) {
+
+    $this->counters[$key] += 1;
+
+    if ($this->counters[$key] > $this->max) {
       return false;
     }
-    
-    $counter[$key] += 1;
-    
+
     return $cache->get($key);
   }
 
