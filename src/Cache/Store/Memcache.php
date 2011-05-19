a2277b042b60573f549c7f58aee09b8f040a9f4e
Adam Brodziak <adam.brodziak@gmail.com>
2011-05-19 12:54:55 +0200

    Hash keys in memcache store (fixes #3702 @1h).
    Key length is limited to 250 chars in memcache, so we have to hash
    long keys in this storage. For now every key is hashed by SHA1.

--- src/Cache/Store/Memcache.php
+++ src/Cache/Store/Memcache.php
@@ -7,31 +7,50 @@ class Cache_Store_Memcache implements Cache_Store {
     $this->memcache = $memcache;
   }
 
+  /**
+   * Hash key due to memcache key length limit (250 chars).
+   * 
+   * See ticket #3702 for details.
+   * @param string $key
+   * @return string
+   */
+  private function prepareKey($key) {
+    return sha1($key);
+  }
+  
   public function get($key) {
+    $key = $this->prepareKey($key);
     return $this->memcache->get($key);
   }
 
   public function set($key, $value, $ttl = 0) {
+    $key = $this->prepareKey($key);
     return $this->memcache->set($key, $value, 0, $ttl);
   }
 
   public function getMulti($keys) {
+    foreach ($keys as &$key) {
+      $key = $this->prepareKey($key);
+    }
     return $this->memcache->get($keys);
   }
 
   public function setMulti($values, $ttl = 0) {
     $result = true;
     foreach($values as $key => $value) {
+      $key = $this->prepareKey($key);
       $result &= $this->set($key, $value, $ttl);
     }
     return (bool)$result;
   }
 
   public function add($key, $value, $ttl = 0) {
+    $key = $this->prepareKey($key);
     return $this->memcache->add($key, $value, 0, $ttl);
   }
 
   public function delete($key) {
+    $key = $this->prepareKey($key);
     // workaround for memcache "delete" various issue
     // for example see comments for http://php.net/manual/en/memcache.delete.php
     return $this->memcache->set($key, false, 0, -1);
