486830e2385a397eb915356c675fc488c3ee7963
Przemek Sobstel <przemek@globalsportsmedia.com>
2012-10-10 13:40:16 +0200

    Re-enable Store::Apc::setMulti() method.

--- src/LSDCache/Store/Apc.php
+++ src/LSDCache/Store/Apc.php
@@ -17,14 +17,11 @@ class Apc implements StoreInterface {
   }
 
   public function setMulti($values, $ttl = 0) {
-    throw new Exception('Not yet implemented');
-    // when setMulti used, apc returns ($key1 => -1, $key2 => -1) array
-
-    // $result = apc_store($values, null, $ttl);
-    // foreach ($result as $key => $value) {
-    //   if ($value < 0) unset($result[$key]);
-    // }
-    // return empty($result);
+    $result = apc_store($values, null, $ttl);
+    foreach ($result as $key => $value) {
+      if ($value < 0) unset($result[$key]);
+    }
+    return empty($result);
   }
 
   public function add($key, $value, $ttl = 0) {
