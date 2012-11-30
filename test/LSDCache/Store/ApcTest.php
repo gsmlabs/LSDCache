026049a724b357421bd333bad22174aa6259a595
Przemek Sobstel <przemek@globalsportsmedia.com>
2012-11-30 14:04:03 +0100

    apc not rerally working in CLI mode, so skip tests

--- test/LSDCache/Store/ApcTest.php
+++ test/LSDCache/Store/ApcTest.php
@@ -5,23 +5,9 @@ use LSDCache\Store;
 class StoreApcTest extends StoreTest {
 
   public function setUp() {
+    $this->markTestIncomplete('APC not really working in CLI mode');
+
     $this->setStore(new Store\Apc());
   }
-  
-  public function testSetAndGetValue() {
-    $this->markTestIncomplete('APC seems not to set value immediately, so it cannot be accessed right after setting');
-  }
 
-  public function testAddMethodSetsValue() {
-    $this->markTestIncomplete('APC seems not to set value immediately, so it cannot be accessed right after setting');
-  }
-
-  public function testAddMethodReturnsFalseOnAlreadyExistingValue() {
-    $this->markTestIncomplete('APC seems not to set value immediately, so it cannot be accessed right after setting');
-  }
-
-  public function testSetMultiAndGetMultiValues() {
-    $this->markTestIncomplete('Functionality not yet implemented');
-  }
-  
 }
