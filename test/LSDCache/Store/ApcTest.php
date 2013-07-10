cf7befb5eef451f38ba8b8526a94a429f830fb21
Milosz Skurowski <milosz.skurowski@globalsportsmedia.com>
2013-07-10 09:25:45 +0100

    Enable CLI mode to have APC working (re #6131)
    APC is not enabled for CLI mode by default. This commit sets up APC dynamically (ini_set) for CLI mode to have tests runnable

--- test/LSDCache/Store/ApcTest.php
+++ test/LSDCache/Store/ApcTest.php
@@ -5,8 +5,27 @@ use LSDCache\Store;
 class StoreApcTest extends StoreTest {
 
   public function setUp() {
-    $this->markTestIncomplete('APC not working in CLI mode');        
+    $isEnabled = $this->enableCliStore();
+    if (false === $isEnabled) {
+      $this->markTestIncomplete('APC not working in CLI mode');
+    }
 
     $this->setStore(new Store\Apc());
+  }
+
+  public function tearDown() {
+    $this->restoreCliStore();
+  }
+
+  protected function enableCliStore() {
+    if (false === (bool)\ini_get('apc.enable_cli')) {
+      \ini_set('apc.enable_cli', 1);
+    }
+
+    return (bool)\ini_get('apc.enable_cli');
+  }
+
+  protected function restoreCliStore() {
+    \ini_restore('apc.enable_cli');
   }
 }
